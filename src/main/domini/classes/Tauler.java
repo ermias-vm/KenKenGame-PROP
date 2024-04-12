98937927e2e1a82a6ca78ffe23274dd6caa6d6ef
david.giribet <david.giribet@estudiantat.upc.edu>
2024-04-12 10:37:17 +0000

    Update Tauler.java

--- src/main/domini/classes/Tauler.java
+++ src/main/domini/classes/Tauler.java
@@ -1,1 +1,25 @@
+package main.domain.classes;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeMap;
+
+public class Tablero {
+    private int idTablero;
+    private int grado;
+    
+    private List<Pieza> piezas;
+
+    public Tablero(int idTablero, int grado) {
+        this.idTablero = idTablero;
+        this.grado = grado;
+        this.piezas = new ArrayList<>();
+    }
+
+    public void agregarPieza(Pieza pieza) {
+        piezas.add(pieza);
+    }
+}
 
