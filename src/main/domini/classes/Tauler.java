2e94c28d6a44cbceef55575ea7c6fb9f884e7459
david.giribet <david.giribet@estudiantat.upc.edu>
2024-04-17 18:21:00 +0200

    Modificat Tauler.java funcions de TaulerJoc.java

--- src/main/domini/classes/Tauler.java
+++ src/main/domini/classes/Tauler.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Tauler {
     private int idTauler;
     private int grau;
-    private List<Regio> regions;
-    private List<Casella> caselles;
+    //private List<Regio> regions;
+    //private List<Casella> caselles;
 
     public Tauler(int idTauler, int grau) {
         this.idTauler = idTauler;
         this.grau = grau;
-        this.regions = new ArrayList<>();
-        this.caselles = new ArrayList<>();
+        //this.regions = new ArrayList<>();
+        //this.caselles = new ArrayList<>();
     }
 
     public int getIdTauler() {
@@ -46,5 +46,25 @@ public class Tauler {
 
     public void esborrarCasella(Casella casella) {
         caselles.remove(casella);
+    }
+
+    public int getValor(int x, int y) {
+        return getCasella(x, y).getValor();
+    }
+
+    public void setValor(int x, int y, int num) {
+        getCasella(x, y).setValor(num);
+    }
+
+    public void borrarValor(int x, int y) {
+        getCasella(x, y).borrarValor();
+    }
+
+    public boolean esModificable(int x, int y) {
+        return getCasella(x, y).esModificable();
+    }
+
+    public boolean esBuida(int x, int y) {
+        return getCasella(x, y).esBuida();
     }
 }
