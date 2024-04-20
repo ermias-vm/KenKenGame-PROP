a85a5c67b69da533b7a56ff21f7bb0d3197dba80
Ermias Valls <ermias.valls@estudiantat.upc.edu@gmail.com>
2024-04-20 14:15:54 +0200

    Delete tests duplicats

--- src/test/CasellaTest.java
+++ src/test/CasellaTest.java
@@ -1,3 +1,4 @@
+package  test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
---
+++ src/test/operacions/ModulTest.java
@@ -0,0 +1,47 @@
+package test.operacions;
+
+import main.domini.classes.operacions.Modul;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModulTest {
+
+    private Modul modul;
+
+    @Before
+    public void setUp() throws Exception {
+        modul = new Modul();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        modul = null;
+    }
+
+    @Test
+    public void opera2() {
+        int result = modul.opera2(10, 3);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void operaN() {
+        int[] valors = {10, 3};
+        int result = modul.operaN(valors);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void calculaPossiblesValors() {
+        // Este método devuelve null en la implementación actual, por lo que no podemos probarlo.
+    }
+
+    @Test
+    public void getNumOperacio() {
+        int numOperacio = modul.getNumOperacio();
+        assertEquals(5, numOperacio);
+    }
+}
