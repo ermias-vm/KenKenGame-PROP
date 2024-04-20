620d8400241924c7b8653cdedc876a6ee7ecc084
Ermias Valls <ermias.valls@estudiantat.upc.edu@gmail.com>
2024-04-21 01:18:00 +0200

    Documentacio classe Casella

--- src/main/domini/classes/Casella.java
+++ src/main/domini/classes/Casella.java
@@ -1,5 +1,12 @@
 package main.domini.classes;
 import main.domini.excepcions.*;
+
+/**
+ * La classe Casella representa una cel·la en un joc de KenKen.
+ * Conté el valor de la cel·la, la seva posició (x, y) i si és modificable o no.
+ *
+ * @author Ermias Valls Mayor
+ */
 public class Casella {
 
     private int valor;
@@ -7,11 +14,13 @@ public class Casella {
     private int posY;
     private boolean modificable;
 
-
     public Casella(){
         this(-1,-1);
     }
 
+    /**
+     * Constructor per defecte. Inicialitza la cel·la amb la posició (-1, -1).
+     */
     public Casella(int x, int y) {
         this.valor = 0;
         this.posX = x;
@@ -19,6 +28,17 @@ public class Casella {
         this.modificable = true;
     }
 
+    /**
+     * Constructor amb posició. Inicialitza la cel·la amb la posició donada i estableix el seu valor a 0 i modificable a true.
+     * @param x La posició x de la cel·la.
+     * @param y La posició y de la cel·la.
+     */
+
+    /**
+     * Estableix el valor de la cel·la.
+     * @param num El valor a establir.
+     * @throws ExcepcioCasellaNoModificable Si la cel·la no és modificable.
+     */
     public void setValor(int num) {
         try {
             if (!modificable) throw new ExcepcioCasellaNoModificable();
@@ -27,6 +47,11 @@ public class Casella {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * Esborra el valor de la cel·la (l'estableix a 0).
+     * @throws ExcepcioCasellaNoModificable Si la cel·la no és modificable.
+     */
     public void borrarValor() {
         try {
             if (!modificable) throw new ExcepcioCasellaNoModificable();
@@ -36,18 +61,36 @@ public class Casella {
         }
     }
 
+    /**
+     * Retorna el valor de la cel·la.
+     * @return El valor de la cel·la.
+     */
     public int getValor(){
         return valor;
     }
 
+    /**
+     * Retorna la posició x de la cel·la.
+     * @return La posició x de la cel·la.
+     */
     public int  getPosX() {
         return this.posX;
     }
 
+    /**
+     * Retorna la posició y de la cel·la.
+     * @return La posició y de la cel·la.
+     */
     public int getPosY() {
         return this.posY;
     }
 
+    /**
+     * Estableix la posició de la cel·la.
+     * @param x La posició x a establir.
+     * @param y La posició y a establir.
+     * @throws ExcepcioCasellaJaTePosicioAssignada Si la cel·la ja té una posició assignada.
+     */
     public void setPosXY(int x, int y) {
         try {
             if (this.posX != -1 && this.posY != -1) throw new ExcepcioCasellaJaTePosicioAssignada();
@@ -59,13 +102,25 @@ public class Casella {
         }
     }
 
+    /**
+     * Estableix la cel·la com a no modificable.
+     */
     public void  setInmodificable () {
         this.modificable = false;
     }
 
+    /**
+     * Retorna si la cel·la és modificable o no.
+     * @return true si la cel·la és modificable, false en cas contrari.
+     */
     public boolean esModificable() {
         return this.modificable;
     }
+
+    /**
+     * Retorna si la cel·la està buida o no.
+     * @return true si la cel·la està buida (el seu valor és 0), false en cas contrari.
+     */
     public boolean esBuida() {
         return this.valor == 0;
     }
