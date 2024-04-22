7e98931b254c0a652ba6c2c560a5eb0d94846a34
nil.beascoechea <nil.beascoechea@estudiantat.upc.edu>
2024-04-22 20:56:57 +0200

    javadoc

---
+++ DOCS/Javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"drivers"},{"l":"main.domini.classes"},{"l":"main.domini.classes.operacions"},{"l":"main.domini.controladors"},{"l":"main.domini.excepcions"},{"l":"main.domini.interficies"},{"l":"main.persistencia"},{"l":"main.stubs"},{"l":"test"},{"l":"test.operacions"}]
