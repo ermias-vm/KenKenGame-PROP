ab578cd08ca4799a73c3b15f2f97e430b2452865
david.giribet <david.giribet@estudiantat.upc.edu>
2024-05-11 20:46:44 +0200

    Vista CrearKenKen per Parametres actualitzada

--- FONTS/src/main/presentacio/CrearKenKenParametres.java
+++ FONTS/src/main/presentacio/CrearKenKenParametres.java
@@ -1,69 +1,70 @@
 package main.presentacio;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CrearKenKenParametres extends JDialog {
     private JTextField textGrau;
-    private JTextField textNOp;
-    private JTextField textOperacions;
-    private JTextPane Operacions;
     private JButton Crea;
     private JButton Cancela;
     private JPanel PanelCreacio;
+    private JCheckBox Suma;
+    private JCheckBox Resta;
+    private JCheckBox Multiplicacio;
+    private JCheckBox Divisio;
+    private JCheckBox Modul;
+    private JCheckBox Exponenciacio;
+    private JLabel ImatgeKenKen;
 
-    public CrearKenKenParametres(JFrame parent){
-        super(parent);
-        setTitle("Create new KenKen");
-        setContentPane(PanelCreacio);
-        setMinimumSize(new Dimension(450,475));
-        setModal(true);
-        setLocationRelativeTo(parent);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
+    public CrearKenKenParametres() {
         Crea.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                createParametres();
+            public void actionPerformed(ActionEvent e) {
+                int grau = Integer.parseInt(textGrau.getText());
+                boolean suma = Suma.isSelected();
+                boolean resta = Resta.isSelected();
+                boolean multiplicacio = Multiplicacio.isSelected();
+                boolean divisio = Divisio.isSelected();
+                boolean modul = Modul.isSelected();
+                boolean exponenciacio = Exponenciacio.isSelected();
+
+                crearKenKen(grau, suma, resta, multiplicacio, divisio, modul, exponenciacio);
             }
         });
+
         Cancela.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                dispose();
+            public void actionPerformed(ActionEvent e) {
+                // Has de canviar-ho a tornar al menu anterior
+                System.exit(0);
             }
         });
-
-        setVisible(true);
-    }
-
-    private void createParametres() {
-        String Grau = textGrau.getText();
-        String NumOp = textNOp.getText();
-        String Operacions = textOperacions.getText();
-
-        if(Grau.isEmpty() || NumOp.isEmpty() || Operacions.isEmpty()){
-            JOptionPane.showMessageDialog(this,
-                    "Plena tots els espais siusplau",
-                    "Torna a provar",
-                    JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-
-        int o = Operacions.length()*2 -1;
-        if(!NumOp.equals(o)){
-            JOptionPane.showMessageDialog(this,
-                    "Les operacions entrades no coincideixen amb el nombre d'operacions que has acordat",
-                    "Torna a provar",
-                    JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-
     }
 
     public static void main(String[] args) {
-        CrearKenKenParametres myForm = new CrearKenKenParametres(null);
+        JFrame frame = new JFrame("Crear KenKen por Parámetros");
+        frame.setContentPane(new CrearKenKenParametres().PanelCreacio);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void crearKenKen(int grau, boolean suma, boolean resta, boolean multiplicacio, boolean divisio, boolean modul, boolean exponenciacio) {
+        // Cridar a CreadorKenkenParam.java
+        System.out.println("Creando KenKen con los siguientes parámetros:");
+        System.out.println("Grado: " + grau);
+        System.out.println("Operaciones:");
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacio);
+        System.out.println("División: " + divisio);
+        System.out.println("Módulo: " + modul);
+        System.out.println("Exponenciación: " + exponenciacio);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        ImatgeKenKen = new JLabel(new ImageIcon("resources/imatges/cuadricula.png"));
     }
 }
