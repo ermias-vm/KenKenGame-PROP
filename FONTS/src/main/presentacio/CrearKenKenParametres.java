3c6d3f41db58edd05354e06be4b9e74870f5ca11
david.giribet <david.giribet@estudiantat.upc.edu>
2024-05-15 16:04:56 +0200

    Vista creacio per parametres funcional

--- FONTS/src/main/presentacio/CrearKenKenParametres.java
+++ FONTS/src/main/presentacio/CrearKenKenParametres.java
@@ -3,20 +3,16 @@ package main.presentacio;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class CrearKenKenParametres extends JDialog {
     //private JTextField textGrau;
     private JButton Crea;
     private JButton Cancela;
     private JPanel PanelCreacio;
-    private JCheckBox Suma;
-    private JCheckBox Resta;
-    private JCheckBox Multiplicacio;
-    private JCheckBox Divisio;
-    private JCheckBox Modul;
-    private JCheckBox Exponenciacio;
     private JLabel ImatgeKenKen;
     private JComboBox ComboBoxGrau;
+    private JComboBox comboBoxOperacions;
 
     public CrearKenKenParametres() {
         System.out.println("Entrant a la pantalla de generar kenken");
@@ -24,12 +20,46 @@ public class CrearKenKenParametres extends JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int grau = Integer.parseInt("" + ComboBoxGrau.getSelectedItem());
-                boolean suma = Suma.isSelected();
-                boolean resta = Resta.isSelected();
-                boolean multiplicacio = Multiplicacio.isSelected();
-                boolean divisio = Divisio.isSelected();
-                boolean modul = Modul.isSelected();
-                boolean exponenciacio = Exponenciacio.isSelected();
+                String operacions = Objects.requireNonNull(comboBoxOperacions.getSelectedItem()).toString();
+                boolean suma = false;
+                boolean resta = false;
+                boolean multiplicacio = false;
+                boolean divisio = false;
+                boolean modul = false;
+                boolean exponenciacio = false;
+                if(operacions.equals("1) Suma")) {
+                    suma = true;
+                }else if(operacions.equals("2) Suma + Resta")) {
+                    suma = true;
+                    resta = true;
+                }else if(operacions.equals("3) Multiplicació + Divisió")) {
+                    multiplicacio = true;
+                    divisio = true;
+                }else if(operacions.equals("4) Opcions 2 i 3 juntes")) {
+                    suma = true;
+                    resta = true;
+                    multiplicacio = true;
+                    divisio = true;
+                }else if(operacions.equals("5) La opció 4 + Modul")) {
+                    suma = true;
+                    resta = true;
+                    multiplicacio = true;
+                    divisio = true;
+                    modul = true;
+                }else if(operacions.equals("6) La opció 4 + Exponenciació")) {
+                    suma = true;
+                    resta = true;
+                    multiplicacio = true;
+                    divisio = true;
+                    exponenciacio = true;
+                }else if(operacions.equals("7) La opció 4 + Modul+ Exponenciació")) {
+                    suma = true;
+                    resta = true;
+                    multiplicacio = true;
+                    divisio = true;
+                    modul = true;
+                    exponenciacio = true;
+                }
 
                 crearKenKen(grau, suma, resta, multiplicacio, divisio, modul, exponenciacio);
             }
@@ -54,10 +84,18 @@ public class CrearKenKenParametres extends JDialog {
 
     private void crearKenKen(int grau, boolean suma, boolean resta, boolean multiplicacio, boolean divisio, boolean modul, boolean exponenciacio) {
 
-        //Aixo treu error si no ha seleccionat cap operacio
-        if(!suma && !resta && !multiplicacio && !divisio && !modul && !exponenciacio){
+        int cont = 0;
+        if(suma) cont++;
+        if(resta) cont++;
+        if(multiplicacio) cont++;
+        if(divisio) cont++;
+        if(modul) cont++;
+        if(exponenciacio) cont++;
+
+        //Aixo treu error si ha seleccionat massa operacions pel grau seleccionat
+        if(cont > grau){
             JOptionPane.showMessageDialog(this,
-                    "Siusplau seleccionar minim una operacio",
+                    "Has seleccionat massa operacions per un grau tant petit",
                     "Torna-ho a provar",
                     JOptionPane.ERROR_MESSAGE);
             return;
